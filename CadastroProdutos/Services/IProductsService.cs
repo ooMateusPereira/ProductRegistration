e440724f84157df649e6a112464bb7b4505f2320
ooMateusPereira <mateusufc1@live.com>
2025-09-18 06:58:52 -0300

    feat: added injection dependency

---
+++ CadastroProdutos/Services/IProductsService.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductRegistration.Services
+{
+    public interface IProductsService
+    {
+        public List<Product> GetAll();
+
+        public Product GetWithId(int id);
+
+        public void AddNewProduct(Product newProduct);
+
+        public Product Update(int id, Product uptadedProduct);
+
+        public bool Delete(int id);
+    }
+}
