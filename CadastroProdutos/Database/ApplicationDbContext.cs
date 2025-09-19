334276c8e8e8c0bf218f732d0e3918756e751b75
ooMateusPereira <mateusufc1@live.com>
2025-09-19 18:13:06 -0300

    feat: added sqlite database

---
+++ CadastroProdutos/Database/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductRegistration.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) 
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
